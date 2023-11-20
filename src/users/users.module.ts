595a8c6e4e72ce05f96e8777c48c318fc6e57e49
Inna3112 <innula3112@gmail.com>
2023-11-20 16:03:09 +0200

    report and user entities added, user dto added, user validation added

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,13 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Module({
+  //this import creates the repository for us
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
