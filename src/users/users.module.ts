d9621ece948fa817d26fea5a9c25cc3cfbbd0a95
Inna3112 <innula3112@gmail.com>
2023-11-25 20:16:19 +0200

    signup method created

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
 import { User } from './user.entity';
 
 @Module({
   //this import creates the repository for us
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, AuthService],
 })
 export class UsersModule {}
