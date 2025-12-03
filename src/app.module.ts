f7fa1d15ec4c2f4e9e1b2906a5cbfed9b5f819b7
Inna3112 <innula3112@gmail.com>
2025-12-03 14:49:48 +0200

    add ormconfig.js for working different envs (development, test, production)

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
 import * as process from 'process';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieSession = require('cookie-session');
@@ -19,17 +17,19 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          entities: [User, Report],
-          synchronize: true, //only for development, it links with migrations
-        };
-      },
-    }),
+    TypeOrmModule.forRoot(), //це потрібно коли ми налаштування перенесли в ormconfig.js
+    // TypeOrmModule.forRootAsync({
+    //   inject: [ConfigService],
+    //   useFactory: (config: ConfigService) => {
+    //     return {
+    //       type: 'sqlite',
+    //       database: config.get<string>('DB_NAME'),
+    //       entities: [User, Report],
+    //       synchronize: true, //only for development, it links with migrations, це робить так щоб кожен раз коли ми запускаємо апп вона сама створює таблиці в базі даних відповідно до наших ентіті
+    //       //якщо видалимо чи додамо поле в ентіті воно видалиться чи додасться у таблиці
+    //     };
+    //   },
+    // }),
     // TypeOrmModule.forRoot({
     //   type: 'sqlite',
     //   database: 'db.sqlite',
