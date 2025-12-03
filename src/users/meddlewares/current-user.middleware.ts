5b172fc77a7b07688b488af9f0ce0f32124c4bb6
Inna3112 <innula3112@gmail.com>
2025-12-03 13:21:38 +0200

    fix ts error

--- src/users/meddlewares/current-user.middleware.ts
+++ src/users/meddlewares/current-user.middleware.ts
@@ -1,11 +1,18 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { UsersService } from '../users.service';
 import { Request, Response, NextFunction } from 'express';
+import { User } from '../user.entity';
 
 //Ми замінили current-user.interceptor на current-user.middleware тому що
 //інтерсептори в nest js виконуються перед або після обробників маршрутів,
 //а AuthGuard виконується до обробників маршрутів.
 //middleware ж виконується до обробників маршрутів і до гвардів,
+
+declare module 'express-serve-static-core' {
+  interface Request {
+    currentUser?: User | null;
+  }
+}
 
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
@@ -16,8 +23,7 @@ export class CurrentUserMiddleware implements NestMiddleware {
 
     if (userId) {
       const user = await this.usersService.findOne(userId);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      //@ts-expect-error
+
       req.currentUser = user;
     }
 
