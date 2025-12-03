ab6830c034711a0933502010a7623f4b689899fa
Inna3112 <innula3112@gmail.com>
2025-12-03 15:13:32 +0200

    add migrations config

--- ormconfig.js
+++ ormconfig.js
@@ -1,9 +1,52 @@
-module.export = {
-  type: 'sqlite',
-  database: 'db.sqlite',
-  entities:
-    process.env.NODE_ENV === 'development'
-      ? ['**/*.entity.js']
-      : ['**/*.entity.ts'],
+// module.export = {
+//   type: 'sqlite',
+//   database: 'db.sqlite',
+//   entities:
+//     process.env.NODE_ENV === 'development'
+//       ? ['**/*.entity.js']
+//       : ['**/*.entity.ts'],
+//   synchronize: false,
+// };
+var dbConfig = {
   synchronize: false,
+  migrations: ['migrations/*.js'],
+  cli: {
+    migrationsDir: 'migrations',
+  },
 };
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.ts'],
+    });
+    break;
+  case 'production':
+    break;
+  default:
+    throw new Error('unknown environment');
+}
+
+module.exports = dbConfig;
+
+//MIGRATIONS COMMANDS
+
+//npm run typeorm migration:generate -- -n MigrationName -schema -o     //-o прапор для того щоб файли міграції генерувалися у js
+
+//npm run typeorm migration:run
+//Шукає всі міграційні файли (зазвичай у migrations/*.ts або migrations/*.js)
+// Визначає, які з них ще не були виконані
+// Виконує їх у правильному порядку
+// Застосовує зміни до бази даних (створення таблиць, зміна колонок, індексів тощо)
+// Записує у спеціальну таблицю migrations інформацію про застосовані міграції
+// ✔ Навіщо це потрібно?
+// Щоб синхронізувати структуру бази даних з вашим кодом (entity) і змінами, які ви робите в проєкті.
