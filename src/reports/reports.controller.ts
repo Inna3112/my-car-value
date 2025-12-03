7277a7fef54057d88039b91a42bc677024c14ea9
Inna3112 <innula3112@gmail.com>
2025-12-03 13:59:59 +0200

    add get-estimate

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,6 +5,8 @@ import {
   UseGuards,
   Patch,
   Param,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
@@ -15,6 +17,7 @@ import { Serialize } from '../interseptors/serialize-interceptor';
 import { ReportDto } from './dtos/report.dto';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -31,5 +34,10 @@ export class ReportsController {
   @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
+  }
+
+  @Get()
+  getEstimate(@Query() guery: GetEstimateDto) {
+    return this.reportsService.createEstimate(guery);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,6 +4,7 @@ import { Report } from './report.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { User } from '../users/user.entity';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
@@ -30,5 +31,26 @@ export class ReportsService {
     report.approved = approved;
 
     return this.repo.save(report);
+  }
+
+  createEstimate(estimateDto: GetEstimateDto) {
+    // return this.repo.createQueryBuilder().select('*').getRawMany(); //поверне усі записи з таблиці reports
+    return (
+      this.repo
+        .createQueryBuilder()
+        // .select('*')
+        .select('AVG(price)', 'price') //щоб повернути середню ціну
+        //додаємо умови фільтрації
+        .where('make = :make', { make: estimateDto.make })
+        .andWhere('model = :model', { model: estimateDto.model }) // вдруге треба використовувавти endWhere тому що другий where перезапише перший
+        .andWhere('lng = :lng BETWEEN -5 AND 5', { lng: estimateDto.lng })
+        .andWhere('lat = :lat BETWEEN -5 AND 5', { lat: estimateDto.lat })
+        .andWhere('year = :year BETWEEN -3 AND 3', { year: estimateDto.year })
+        .andWhere('approved IS TRUE')
+        .orderBy('ABS(mileage = :mileage)', 'DESC')
+        .setParameter('mileage', estimateDto.mileage)
+        .limit(3) //щоб узяти перші 3 записи
+        .getRawMany()
+    );
   }
 }
